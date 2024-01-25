0fccd7835b93eaba09bc94a6221697c09729b340
BGood8806 <156936708+BGood8806@users.noreply.github.com>
2024-01-25 15:20:41 -0500

    Add files via upload
    This code works with functions of logical operators if, else, elif, == to provide a gaming experience of choosing your own adventure

---
+++ Day3ChooseYourAdventureGame.py
@@ -0,0 +1,48 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+#The code for choosing left or right
+direction = input ("Will you take your adventures to the left or right?").lower()
+if direction == "left":
+  decision = input("Should we swim or should we wait for the beast to leave?").lower()
+#The code for choosing to wait or swim
+  if decision == "wait":
+    door = input("Which door will you take to find the buried treasure?\n Red, Blue, or Yellow?").lower()
+#The code for choosing red, blue, or yellow door
+    if door == "yellow":
+      print("You have found the buried treasure, Congratulations!!!")
+    elif door == "blue":
+      print ("A group of behemoth awaits you and have attacked you.\nGame Over!")
+    elif door == "red":
+      print("You were engulfed by a fireball and the room became fire.\n Game Over!")
+    else: print("You have fallen into a hole.\n Game Over!!")
+  else:
+      print("You were attacked by giant trout and eaten alive.\n Game Over!!!")
+else:
+  print("Game Over!")
